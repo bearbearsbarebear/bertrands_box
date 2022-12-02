7d027d68a0939e6dfa1651848382b13f380479f9
Bear <69819027+bearbearsbarebear@users.noreply.github.com>
2022-12-02 17:29:40 -0300

    Refactoring
    Addition of comments and removal of bad practices.

--- snail_sort.c
+++ snail_sort.c
@@ -1,56 +1,48 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-#define SIZE 3
-
-/*
-snail sort:
-array = [[1,2,3],
-         [4,5,6],
-         [7,8,9]]
-snail(array) #=> [1,2,3,6,9,8,7,4,5]
-*/
-
-// rows=cols
-int* snail(size_t* outsz, int** mx, size_t rows, size_t cols)
+// snail is a function that takes a two-dimensional array and returns
+// an array containing the elements of the input array in a "snail"
+// pattern, starting from the top left corner and spiraling inwards.
+int* snail(size_t* outsz, int** mx, size_t rows, size_t cols) 
 {
+	// Declare variables for the current row and column in the snail traversal.
 	size_t vertical, horizontal;
-	int* output = (int*) calloc((*outsz = rows*rows), sizeof(int));
 
-	// Setting initial value before starting the loop
+	// Allocate memory for the output array, which will have the same number
+	// of elements as the input array.
+	int* output = (int*) malloc((*outsz = rows*rows) * sizeof(int));
+	if (output == NULL) {
+		printf("Error allocating memory for output array\n");
+		return NULL;
+	}
+
+	// Initialize the current row and column to the top left corner of the input array.
 	vertical = horizontal = rows = 0;
+
+	// Decrement the number of columns, since we will be using this variable as an
+	// upper bound for the column index in the traversal loops below.
 	cols--;
 
+	// Perform the snail traversal. The traversal will continue until the current
+	// row reaches the last column of the input array.
 	while (rows < cols) {
-		// Walks towards east (stops at column limit)
+		// Traverse the current row from left to right, adding each element to the output array.
 		while (horizontal < cols) *output++ = mx[vertical][horizontal++];
-
-		// Walks toward south (stops at column limit)
+		// Traverse the current column from top to bottom, adding each element to the output array.
 		while (vertical < cols) *output++ = mx[vertical++][horizontal];
-
-		// Walks toward west (stops at row limit)
+		// Traverse the current row from right to left, adding each element to the output array.
 		while (horizontal > rows) *output++ = mx[vertical][horizontal--];
-
-		// Walks toward north (stops at row limit)
+		// Traverse the current column from bottom to top, adding each element to the output array.
 		while (vertical > rows) *output++ = mx[vertical--][horizontal];
 
-		/*
-		 *When it's here, it means it finished a loop;
-		 *So what it does is it jumps the loop
-		 *further inside, which means closing
-		 *the outer part of the matrix and
-		 *starting at the upper-left part of the
-		 *remaining matrix
-		 **********************************************
-		 *Vertical and Horizontal starts at (0, 0) then
-		 *jumps to (1, 1) etc
-		 *Rows starting at 0 and cols at SIZE
-		 *Since the idea is closing more and more 
-		 *Towards the inner side, add rows and subs cols
-		 *When both are equal, means it's finished
-		*/
+		// Move to the next outer layer of the input array by incrementing the current row and column,
+		// and decrementing the number of rows and columns.
 		vertical++, horizontal++, rows++, cols--;
 	}
 
+	// If the number of rows and columns is the same, we have reached the middle of the input array.
+	// In this case, add the element at the current row and column to the output array.
 	if (rows == cols) *output++ = mx[vertical][horizontal];
 
 	return output - *outsz;
@@ -59,22 +51,48 @@ int* snail(size_t* outsz, int** mx, size_t rows, size_t cols)
 int main(int argc, char* argv[])
 {
 	size_t size;
-	int** test_array;
 
-	// Just creating and filling test_array
-	test_array = (int**) malloc(sizeof(int*) * SIZE);
-	for (int i = 0; i < SIZE; ++i) {
-		test_array[i] = (int*) malloc(sizeof(int) * SIZE);
+	// Declare a variable for the number of rows and columns in the input array.
+	int n = 4;
+
+	int** test_array = (int**) malloc(sizeof(int*) * n);
+	if (test_array == NULL) {
+		printf("Error allocating memory for test_array\n");
+		return -1;
 	}
 
-	for (int i = 0; i < SIZE; ++i) {
-		for (int j = 0; j < SIZE; ++j) {
-			test_array[i][j] = (i*SIZE) + j + 1;
+	for (int i = 0; i < n; ++i) {
+		test_array[i] = (int*) malloc(sizeof(int) * n);
+		if (test_array[i] == NULL) {
+			printf("Error allocating memory for test_array[%d]\n", i);
+			return -1;
 		}
 	}
 
-	// Resultant snail sorted array
-	int* array = snail(&size, test_array, SIZE, SIZE);
+	// Initialize the elements of the input array.
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			test_array[i][j] = (i*n) + j + 1;
+		}
+	}
+
+	// Call the snail function to get the snail traversal of the input array.
+	int* output = snail(&size, test_array, n, n);
+
+	for (int i = 0; i < n*n; ++i) {
+		printf("%d ", output[i]);
+	}
+
+	// Free the memory allocated for the output array.
+	free(output);
+
+	// Free the memory allocated for each row of the input array.
+	for (int i = 0; i < n; i++) {
+		free(test_array[i]);
+	}
+
+	// Free the memory allocated for the input array.
+	free(test_array);
 
 	return 0;
 }
