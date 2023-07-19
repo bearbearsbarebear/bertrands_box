591e22efb784fc6842f0fa542f3d0bf150368be3
Bear <69819027+bearbearsbarebear@users.noreply.github.com>
2023-07-19 10:38:20 -0300

    Fixing indentation

--- birthday_problem.cpp
+++ birthday_problem.cpp
@@ -11,22 +11,19 @@
  * birthday exceeds 50% in a group of only 23 people.
 */
 
-bool is_date_valid(int month, int day) {
-  if (month < 1 || month > 12) {
-    return false;
-  }
-
-  if (day < 1 || day > 31) {
-    return false;
-  }
-
-  if (month == 2) {
-    return day <= 28;
-  } else if (month == 4 || month == 6 || month == 9 || month == 11) {
-    return day <= 30;
-  }
-
-  return true;
+bool is_date_valid(int month, int day)
+{
+	if (month < 1 || month > 12) return false;
+	
+	if (day < 1 || day > 31) return false;
+	
+	if (month == 2) {
+		return day <= 28;
+	} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+		return day <= 30;
+	}
+	
+	return true;
 }
 
 int main(int argc, char* argv[])
